d02ca01c23a31837ecdcda8c8b6efd755579ab1d
verbaux <verbaux@yandex.ru>
2020-03-15 13:19:04 +0300

    Module2-task3 Add server

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,6 +4,8 @@ const DEFAULT_COMMAND = `--help`;
 
 const DEFAULT_COUNT = 1;
 
+const DEFAULT_PORT = 3000;
+
 const FILE_NAME = `mocks.json`;
 
 const FILE_SENTENCES_PATH = `./data/sentences.txt`;
@@ -37,9 +39,18 @@ const PictureRestrict = {
   max: 16,
 };
 
+const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
+
 module.exports = {
   DEFAULT_COMMAND,
   DEFAULT_COUNT,
+  DEFAULT_PORT,
   FILE_NAME,
   FILE_SENTENCES_PATH,
   FILE_TITLES_PATH,
@@ -49,5 +60,6 @@ module.exports = {
   ExitCode,
   OfferType,
   SumRestrict,
-  PictureRestrict
+  PictureRestrict,
+  HttpCode
 };
