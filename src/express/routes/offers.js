faf643d750ed840b1863ed57e1af5031ac1d29b0
verbaux <verbaux@yandex.ru>
2020-03-29 19:33:10 +0300

    Module3-task2 Add static and pug templates

--- src/express/routes/offers.js
+++ src/express/routes/offers.js
@@ -3,9 +3,9 @@
 const {Router} = require(`express`);
 const offersRouter = new Router();
 
-offersRouter.get(`/add`, (req, res) => res.send(`/offers/add`));
-offersRouter.get(`/:id`, (req, res) => res.send(`/offers/${req.params.id}`));
-offersRouter.get(`/edit/:id`, (req, res) => res.send(`/offers/edit/${req.params.id}`));
-offersRouter.get(`/category/:id`, (req, res) => res.send(`/offers/category/${req.params.id}`));
+offersRouter.get(`/add`, (req, res) => res.render(`new-tickets`));
+offersRouter.get(`/:id`, (req, res) => res.render(`ticket`));
+offersRouter.get(`/edit/:id`, (req, res) => res.render(`ticket-edit`));
+offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
 
 module.exports = offersRouter;
